458efdffc12d4d2ca3e7480dabcfa814bbe4bbc9
Adhitya B <adhiadhi3129@gmail.com>
2025-09-05 09:35:01 +0530

    Day 2 Assignment 3

---
+++ JavaProject1/src/com/TCF/JFS/Assignment/thisKeyword/BookStore.java
@@ -0,0 +1,7 @@
+package com.TCF.JFS.Assignment.thisKeyword;
+
+public class BookStore {
+	public void addBook(Book b) {
+        System.out.println("Book added to store: " + b.title + " by " + b.author);
+    }
+}
